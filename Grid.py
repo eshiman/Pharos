beade352401a6987e33f72d23a4f4434104a5852
Esther <eshiman@gmail.com>
2017-02-28 18:16:11 -0500

    Set up for localizer

---
+++ Grid.py
@@ -0,0 +1,12 @@
+'''
+Authors: Esther Shimanovich, Jeremy Savarin and Abbas Furniturewalla
+
+Stores the Grid in a list of lists.
+'''
+
+
+class grid:
+
+    GridMap = []
+
+    def __init__(self):
---
+++ Localizer.py
@@ -0,0 +1,11 @@
+'''
+Authors: Esther Shimanovich, Jeremy Savarin and Abbas Furniturewalla
+
+Calculates the location of the third device using the cosine formula.
+a^2 = b^2 + c^2 - 2bc(cosA)
+'''
+
+
+class Localizer:
+
+    def __init__(self):
