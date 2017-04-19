0758e67d28a4cb0eff77cd2b0811bd48aa9ea4cc
Esther <eshiman@gmail.com>
2017-04-19 14:23:52 -0400

    serial python change

--- arduinoSerialData/arduinoSerial.py
+++ arduinoSerialData/arduinoSerial.py
@@ -1,7 +1,6 @@
 import serial  # Import Serial Library
 
-arduinoSerialData = serial.Serial('\\.\COM6', 9600)  # Create Serial port object called arduinoSerialData
-arduinoSerialData.close();
+arduinoSerialData = serial.Serial('\\.\COM6', 115200)  # Create Serial port object called arduinoSerialData
 # COM6 == 5
 while (1 == 1):
     if arduinoSerialData.inWaiting() > 0:
